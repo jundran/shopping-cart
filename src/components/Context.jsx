6881e49a899a0b2e13ca477f1168b6f5e9ad0cb1
jundran <jundran@example.com>
2023-04-02 19:18:23 -0600

    Use local storage

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,12 +1,24 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 const CartContext = createContext()
 
 export default function CartProvider ({ children }) {
 	const [items, setItems] = useState([])
 
+	useEffect(() => {
+		console.log('Getting saved items from local storage')
+		const savedItems = JSON.parse(localStorage.getItem('shopping-cart'))
+		if (savedItems) setItems(savedItems)
+	}, [])
+
+	function updateLocalStorage (updatedItems = items) {
+		console.log('Updating local storage')
+		localStorage.setItem('shopping-cart', JSON.stringify(updatedItems))
+	}
+
 	function clearCart () {
 		setItems([])
+		updateLocalStorage([])
 	}
 
 	function findItem (id) {
@@ -24,6 +36,7 @@ export default function CartProvider ({ children }) {
 		const newItems = items.filter(item => item.id !== id)
 		newItems.push(updatedItem)
 		setItems(newItems)
+		updateLocalStorage(newItems)
 	}
 
 	return (
